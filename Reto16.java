0b7ac3ad5181b44dbe0ecd70bf49417573cf5c36
valeriabarcenas04 <153239420+valeriabarcenas04@users.noreply.github.com>
2026-06-16 10:49:22 -0600

    Operación.java

---
+++ Reto16.java
@@ -0,0 +1,8 @@
+public class Reto16 {
+    public static void main(String[] args) {
+
+        double resultado = Math.pow((3.0 + 2.0) / (2.0 * 5.0), 2);
+
+        System.out.println("Resultado: " + resultado);
+    }
+}
