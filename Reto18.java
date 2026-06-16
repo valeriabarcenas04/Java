9723dcb991d89ffbe1be9d8a9164d70ea12dbf39
valeriabarcenas04 <153239420+valeriabarcenas04@users.noreply.github.com>
2026-06-16 10:37:19 -0600

    Perimetro de circulo

---
+++ Reto18.java
@@ -0,0 +1,13 @@
+public class Reto18 {
+    public static void main(String[] args) {
+
+        double radio = 5.0;
+
+        double area = Math.PI * Math.pow(radio, 2);
+        double perimetro = 2 * Math.PI * radio;
+
+        System.out.println("Radio: " + radio);
+        System.out.println("Área: " + area);
+        System.out.println("Perímetro: " + perimetro);
+    }
+}
