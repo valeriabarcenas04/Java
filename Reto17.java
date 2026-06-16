4046c43ec9dbe4d3be4784ab07f720db7afc785f
valeriabarcenas04 <153239420+valeriabarcenas04@users.noreply.github.com>
2026-06-16 10:46:46 -0600

    Decimal y Residuo

---
+++ Reto17.java
@@ -0,0 +1,13 @@
+public class Reto17 {
+    public static void main(String[] args) {
+        int n = 17;
+        int m = 5;
+
+        int c = n / m; 
+        int r = n % m; 
+
+        System.out.println(n + " entre " + m +
+                           " da un cociente " + c +
+                           " y un residuo " + r);
+    }
+}
