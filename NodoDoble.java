0e64085728b2172260c296841f51883c7bb81957
valeriabarcenas04 <153239420+valeriabarcenas04@users.noreply.github.com>
2026-07-14 10:48:42 -0600

    NodoDoble.java

---
+++ NodoDoble.java
@@ -0,0 +1,11 @@
+class NodoDoble {
+    String dato;
+    NodoDoble siguiente;
+    NodoDoble anterior;
+
+    public NodoDoble(String dato) {
+        this.dato = dato;
+        this.siguiente = null;
+        this.anterior = null;
+    }
+}
