8bbd4588b5b818b1027572deb3de615737ed050f
valeriabarcenas04 <153239420+valeriabarcenas04@users.noreply.github.com>
2026-07-22 13:21:50 -0600

    MetodoRecursivo.java

---
+++ MetodoRecursivo.java
@@ -0,0 +1,18 @@
+public class MetodoRecursivo {
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+    public static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
