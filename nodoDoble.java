6b68f1e0529173657667fb3c1486511b82756ac2
valeriabarcenas04 <153239420+valeriabarcenas04@users.noreply.github.com>
2026-07-22 13:40:34 -0600

    nodoDoble.java

---
+++ nodoDoble.java
@@ -0,0 +1,13 @@
+public class NodoDoble {
+
+    Curso curso;
+    NodoDoble siguiente;
+    NodoDoble anterior;
+
+    public NodoDoble(Curso curso) {
+        this.curso = curso;
+        this.siguiente = null;
+        this.anterior = null;
+    }
+
+}
