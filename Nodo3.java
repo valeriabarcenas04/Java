04861aeb5b20f4fb324e5074d468f120d4e27c1e
valeriabarcenas04 <153239420+valeriabarcenas04@users.noreply.github.com>
2026-08-06 10:07:02 -0600

    Nodo3.java

---
+++ Nodo3.java
@@ -0,0 +1,12 @@
+public class Nodo {
+
+    int dato;
+    Nodo derecho;
+    Nodo izquierdo;
+
+    public Nodo(int dato) {
+        this.dato = dato;
+        this.izquierdo = null;
+        this.derecho = null;
+    }
+}
